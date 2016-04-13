bccd70ff9f016664f27636192223abcdd3740acc
Jonh Wendell <jonh.wendell@redhat.com>
2016-04-13 00:47:45 -0300

    wip2

--- eap/eap64/src/test/java/org/jboss/test/arquillian/ce/eap64/Eap64ClusterTest.java
+++ eap/eap64/src/test/java/org/jboss/test/arquillian/ce/eap64/Eap64ClusterTest.java
@@ -1,15 +1,22 @@
 package org.jboss.test.arquillian.ce.eap64;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Logger;
 
+import org.jboss.arquillian.ce.api.ConfigurationHandle;
 import org.jboss.arquillian.ce.api.OpenShiftHandle;
+import org.jboss.arquillian.ce.api.OpenShiftResource;
+import org.jboss.arquillian.ce.api.RoleBinding;
+import org.jboss.arquillian.ce.api.RoleBindings;
 import org.jboss.arquillian.ce.api.Template;
 import org.jboss.arquillian.ce.api.TemplateParameter;
 import org.jboss.arquillian.ce.cube.RouteURL;
@@ -32,39 +39,64 @@ import org.junit.runner.RunWith;
 @Template(url = "https://raw.githubusercontent.com/jboss-openshift/application-templates/master/eap/eap64-basic-s2i.json",
 parameters = {
         @TemplateParameter(name = "SOURCE_REPOSITORY_URL", value="https://github.com/jwendell/temp"),
-        @TemplateParameter(name = "SOURCE_REPOSITORY_REF", value="master"),
+        @TemplateParameter(name = "SOURCE_REPOSITORY_REF", value="t1"),
         @TemplateParameter(name = "CONTEXT_DIR", value="cluster1")
         })
+@OpenShiftResource("classpath:eap-app-secret.json")
+@RoleBindings({
+        @RoleBinding(roleRefName = "view", userName = "system:serviceaccount:${kubernetes.namespace}:default"),
+        @RoleBinding(roleRefName = "view", userName = "system:serviceaccount:${kubernetes.namespace}:eap-service-account")
+})
 public class Eap64ClusterTest {
     private static final Logger log = Logger.getLogger(Eap64ClusterTest.class.getName());
-
-    @RouteURL("eap-app")
-    private URL url;
 
     @ArquillianResource
     OpenShiftHandle adapter;
 
+    @ArquillianResource
+    ConfigurationHandle config;
+
     @Test
     @RunAsClient
-    @Ignore
-	public void dummyTest() throws Exception {
-        Map<String, String> labels = new HashMap<>();
-        labels.put("application", "eap-app");
-        labels.put("deploymentConfig", "eap-app");
+	public void testSession() throws Exception {
+        final String token = config.getToken();
+        assertFalse(token.isEmpty());
+
         adapter.scaleDeployment("eap-app", 2);
-        InputStream response = adapter.execute(labels, 0, 8080, "/cluster1/Hi");
-        log.info("GOT RESPONSE 1 " + readInputStream(response));
+        List<String> pods = adapter.getPods();
+        pods.removeIf(p -> p.endsWith("-build") || !p.startsWith("eap-app-"));
 
-        response = adapter.execute(labels, 1, 8080, "/cluster1/Hi");
-        log.info("GOT RESPONSE 2 " + readInputStream(response));
+        HttpClient client = HttpClientBuilder.untrustedConnectionClient();
+        final String value = UUID.randomUUID().toString();
 
-        response = adapter.execute(labels, 2, 8080, "/cluster1/Hi");
-        log.info("GOT RESPONSE 3 (MUST FAIL): " + readInputStream(response));
+        // Insert a session value into the first pod
+        String servletUrl = buildURL(pods.get(0));
+        HttpRequest request = HttpClientBuilder.doPOST(servletUrl);
+        request.setHeader("Authorization", "Bearer " + token);
+        Map<String, String> params = new HashMap<>();
+        params.put("key", value);
+        request.setEntity(params);
+        HttpResponse response = client.execute(request);
+        assertEquals("OK", response.getResponseBodyAsString());
+
+        // Retrieve the value from the second pod
+        servletUrl = buildURL(pods.get(1));
+        request = HttpClientBuilder.doGET(servletUrl);
+        request.setHeader("Authorization", "Bearer " + token);
+        request.setHeader("Cookie", response.getHeader("Set-Cookie"));
+        response = client.execute(request);
+        assertEquals(value, response.getResponseBodyAsString());
+    }
+
+    private String buildURL(String podName) {
+        final String PROXY_URL = "%s/api/%s/namespaces/%s/pods/%s:%s/proxy%s";
+        return String.format(PROXY_URL, config.getKubernetesMaster(), config.getApiVersion(), config.getNamespace(), podName, 8080, "/cluster1/StoreInSession");
     }
 
     @Test
     @RunAsClient
-    public void testAppStillWorksWhenScalingDown() throws Exception {
+    @Ignore
+    public void testAppStillWorksWhenScalingDown(@RouteURL("eap-app") URL url) throws Exception {
         // Number of HTTP requests we are going to do
         final int REQUESTS = 100;
         // Initial number of replicas, will decrease over time until 1
